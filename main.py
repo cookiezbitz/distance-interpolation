fab0d160985c962f5c3593c4bcae690e43084a66
Remiel Lin <74459428+cookiezbitz@users.noreply.github.com>
2024-07-03 13:20:19 -0700

    Camera Access

--- main.py
+++ main.py
@@ -6,5 +6,25 @@ import matplotlib.pyplot as plt
 import os
 
 alive = True
+win_name = "Camera Filters"
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+result = None
 
 source = cv2.VideoCapture(0)
+key = cv2.waitKey(1)
+
+while alive:
+    ret, frame = source.read()
+    cv2.imshow('Video', frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        alive = False
+        break
+    elif key == ord('s'):
+        cv2.imwrite('screenshot.png', frame)
+    elif key == ord('f'):
+        cv2.imwrite('filtered.png', result)
+
+
+source.release()
+cv2.destroyWindow(win_name)
