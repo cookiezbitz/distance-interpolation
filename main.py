e93b92de40b8765b91070a2e13c559e97a47bcb7
Remiel Lin <74459428+cookiezbitz@users.noreply.github.com>
2024-07-07 13:35:40 -0700

    Both Cameras Initialized

--- main.py
+++ main.py
@@ -9,30 +9,35 @@ import os
 import sys
 from picamera2 import Picamera2, Preview
 
-
-
-# Initialize Picamera2
-cam1 = Picamera2()
-preview_config = cam1.create_preview_configuration()
-cam1.configure(preview_config)
-cam1.start()
-
+# Defining Variables
 alive = True
-win_name = "Camera Filters"
-cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+win_name1 = "Right Camera"
+win_name2 = "Left Camera"
+
+# Initialize cameras
+cam1 = Picamera2(0)
+cam2 = Picamera2(1)
+preview_config = cam1.create_preview_configuration()
+preview_config2 = cam2.create_preview_configuration()
+cam1.configure(preview_config)
+cam2.configure(preview_config2)
+cam1.start()
+cam2.start()
 
 
-
-#source = cv2.VideoCapture(0)
-key = cv2.waitKey(1)
+cv2.namedWindow(win_name1, cv2.WINDOW_NORMAL)
+cv2.namedWindow(win_name2, cv2.WINDOW_NORMAL)
 
 while alive:
     # Capture an image with picamera2
     frame = cam1.capture_array()
+    frame2 = cam2.capture_array()
     # Convert the image into a format OpenCV can use
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_RGB2BGR)
     # Display the image
-    cv2.imshow(win_name, frame)
+    cv2.imshow(win_name1, frame)
+    cv2.imshow(win_name2, frame2)
     key = cv2.waitKey(1)
     if key == ord('q'):
         alive = False
