794b01b3b115a7c586e39335e886bbfaa70d501f
Remiel <remiellin0.0@gmail.com>
2024-07-08 13:16:16 -0700

    Camera Test success, bug fix in works

--- cameratest.py
+++ cameratest.py
@@ -1,29 +1,71 @@
+"""SETTINGS AND VARIABLES ________________________________________________________________"""
+
+## This is intended to work on linux systems
+import numpy
 import cv2
-import time
+import matplotlib.pyplot as plt
+from time import sleep
+import os
+import sys
+from picamera2 import Picamera2, Preview
 
-source = cv2.VideoCapture(0, cv2.CAP_V4L)
+# Defining Variables
+alive = True
+win_name1 = "Right Camera"
+win_name2 = "Left Camera"
+firstStart = True
 
-# Add a warm-up period
-time.sleep(2)  # Wait for 2 seconds
+# canny thresholds
+cannylow = 100
+cannyhigh = 200
 
-# Optionally, set a specific resolution
-source.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-source.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Initialize cameras
+cam1 = Picamera2(0)
+cam2 = Picamera2(1)
+preview_config = cam1.create_preview_configuration()
+preview_config2 = cam2.create_preview_configuration()
+cam1.configure(preview_config)
+cam2.configure(preview_config2)
+cam1.start()
+cam2.start()
 
-# Attempt to capture a frame multiple times
-for _ in range(10):
-    ret, frame = source.read()
-    if ret:
+cv2.namedWindow(win_name1, cv2.WINDOW_NORMAL)
+cv2.namedWindow(win_name2, cv2.WINDOW_NORMAL)
+
+while alive:
+    # Capture an image with picamera2
+    frame = cam1.capture_array()
+    frame2 = cam2.capture_array()
+    # Convert the image into a format OpenCV can use
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_RGB2BGR)
+    if(firstStart):
+        filteredImage1 = frame
+        filteredImage2 = frame2
+        firstStart = False
+    # Display the image
+    cv2.imshow(win_name1, filteredImage1)
+    cv2.imshow(win_name2, filteredImage2)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        alive = False
         break
-    time.sleep(0.1)  # Wait a bit before retrying
+    elif key == ord("s"):
+        cv2.imwrite("screenshot.png", frame)
+        # takes a snapshot
+    elif key == ord("n"):
+        filteredImage1 = frame
+        filteredImage2 = frame2
+        print("take it back now y'all")
+    elif key == ord("c"):
+        filteredImage1 = cv2.Canny(frame, cannylow, cannyhigh)
+        filteredImage2 = cv2.Canny(frame2, cannylow, cannyhigh)
+        print("canny!")
+    elif key == ord("b"):
+        filteredImage1 = cv2.blur(frame, (5, 5))
+        filteredImage2 = cv2.blur(frame2, (5, 5))
+        print("blur!")
 
-if not ret:
-    print(
-        "Failed to capture frame after multiple attempts. Camera may not be accessible or index may be incorrect."
-    )
-else:
-    cv2.imshow("Test Frame", frame)
-    cv2.waitKey(0)  # Wait for a key press to close the window
 
-source.release()
+cam1.stop()
 cv2.destroyAllWindows()
